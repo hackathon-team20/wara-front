261274ffeeafea3c09ab20880fe286a77ccddf06
kametsun <kametsun7@gmail>
2024-06-03 23:31:05 +0900

    [add] TextForm component

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,19 +1,24 @@
 "use client"
 
-import {Box, Container, TextField, Typography} from "@mui/material";
+import {Box, Container, Typography} from "@mui/material";
 import Button from "@mui/material/Button";
 import {useState} from "react";
+import TextForm from "@/app/signup/TextForm";
 
 export default function SignUpPage() {
     const [step, setStep] = useState(1)
+    const [name, setName] = useState("")
     const [email, setEmail] = useState("")
-    const [error, setError] = useState("")
+    const [password, setPassword] = useState("")
 
     /**
      * emailの重複チェックを行う
+     * 重複してない -> step2: 名前とパスワードの入力画面
+     * 重複してる -> そのままで新規パスワードを入力してもらう
      */
     const handleSubmitEmail = () => {
         console.log(`email ${email}`)
+        setStep(2)
     }
 
     /**
@@ -21,6 +26,43 @@ export default function SignUpPage() {
      */
     const handleSignUp = () => {
         console.log("登録処理をする")
+        console.log(`名前: ${name}`)
+        console.log(`パスワード: ${password}`)
+    }
+
+    /**
+     * ステップ1で表示されるメールアドレスフォーム
+     */
+    const emailForm = () => {
+        return (
+            <>
+                <Typography variant="h6" component="h2" py={2} fontWeight={"bolder"}>
+                    アカウントを作成する
+                </Typography>
+                <Typography pb={2}>
+                    メールアドレスを入力してください
+                </Typography>
+                <TextForm label={"メールアドレス"} onChange={setEmail} type={"email"}/>
+            </>
+        )
+    }
+
+    /**
+     * ステップ2で表示されるフォーム
+     */
+    const signUpForm = () => {
+        return (
+            <>
+                <Typography variant="h6" component="h2" py={2} fontWeight={"bolder"}>
+                    続ける
+                </Typography>
+                <Typography pb={2}>
+                    名前とパスワードを入力してください
+                </Typography>
+                <TextForm label={"名前"} onChange={setName} type={"text"}/>
+                <TextForm label={"パスワード"} onChange={setPassword} type={"password"}/>
+            </>
+        )
     }
 
     return (
@@ -38,38 +80,15 @@ export default function SignUpPage() {
                     <Typography variant="h3" component="h3" pb={15} fontWeight={"bold"}>
                         アプリ名(仮)
                     </Typography>
-
-                    {step === 1 && (
-                        <Box width={"100%"} display={"flex"} flexDirection={"column"} justifyContent={"center"}
-                             alignItems={"center"}>
-                            <Typography variant="h6" component="h2" py={2} fontWeight={"bolder"}>
-                                アカウントを作成する
-                            </Typography>
-                            <Typography pb={2}>
-                                メールアドレスを入力してください
-                            </Typography>
-                            <TextField
-                                fullWidth
-                                label="メールアドレス"
-                                onChange={(e) => setEmail(e.target.value)}
-                                sx={{
-                                    mt: 3,
-                                    '& .MuiOutlinedInput-root': {
-                                        borderRadius: '10px',
-                                        '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                                            borderColor: 'gray',
-                                        },
-                                    },
-                                    '& .MuiInputLabel-root': {
-                                        color: 'gray',
-                                    },
-                                    '& .Mui-focused .MuiInputLabel-root': {
-                                        color: 'gray',
-                                    },
-                                }}
-                            />
-                        </Box>
-                    )}
+                    <Box width={"100%"} display={"flex"} flexDirection={"column"} justifyContent={"center"}
+                         alignItems={"center"}>
+                        {step === 1 && (
+                            emailForm()
+                        )}
+                        {step === 2 && (
+                            signUpForm()
+                        )}
+                    </Box>
                     <Button
                         fullWidth
                         sx={{
@@ -81,9 +100,9 @@ export default function SignUpPage() {
                                 backgroundColor: 'black',
                             },
                         }}
-                        onClick={handleSubmitEmail}
+                        onClick={step === 1 ? handleSubmitEmail : handleSignUp}
                     >
-                        Check Email
+                        {step === 1 ? "Check Email" : "Sign Up"}
                     </Button>
                 </Box>
             </Container>
