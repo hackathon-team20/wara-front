283b381b99f5ea7dd3e3b926bb50925135c2b53c
tomizawakenshin <kenshin.8799@gmail.com>
2024-06-06 19:18:46 +0900

    API通さない方法とAPI通して実行する方法のどちらでも実行可能なようにした

--- app/star/page.tsx
+++ app/star/page.tsx
@@ -9,30 +9,32 @@ import ShowTop3 from '../components/ShowTop3';
 import Show4thOrLower from '../components/Show4thOrLower';
 
 const page = async () => {
-    const Users_api = await getAllUserRanking();
+    //ちゃんとapi通してやるならこっちのコメントアウトを外す
+    //const Users_api = await getAllUserRanking();
 
-    // const Users = [
-    //     { userid: 1, username: 'user1', HeartPoints: 100, usericon: '/GHOSPOT.png' },
-    //     { userid: 2, username: 'user2', HeartPoints: 90, usericon: '/GHOSPOT.png' },
-    //     { userid: 3, username: 'user3', HeartPoints: 80, usericon: '/GHOSPOT.png' },
-    //     { userid: 4, username: 'user4', HeartPoints: 70, usericon: '/GHOSPOT.png' },
-    //     { userid: 5, username: 'user5', HeartPoints: 60, usericon: '/GHOSPOT.png' },
-    //     { userid: 6, username: 'user6', HeartPoints: 50, usericon: '/GHOSPOT.png' },
-    //     { userid: 7, username: 'user7', HeartPoints: 40, usericon: '/GHOSPOT.png' },
-    //     { userid: 8, username: 'user8', HeartPoints: 30, usericon: '/GHOSPOT.png' },
-    //     { userid: 9, username: 'user9', HeartPoints: 20, usericon: '/GHOSPOT.png' },
-    //     { userid: 10, username: 'user10', HeartPoints: 10, usericon: '/GHOSPOT.png' },
-    //     { userid: 11, username: 'user11', HeartPoints: 10, usericon: '/GHOSPOT.png' },
-    //     { userid: 12, username: 'user12', HeartPoints: 10, usericon: '/GHOSPOT.png' },
-    //     { userid: 13, username: 'user13', HeartPoints: 10, usericon: '/GHOSPOT.png' },
-    //     { userid: 14, username: 'user14', HeartPoints: 10, usericon: '/GHOSPOT.png' },
-    //     { userid: 15, username: 'user15', HeartPoints: 10, usericon: '/GHOSPOT.png' },
-    //     { userid: 16, username: 'user16', HeartPoints: 10, usericon: '/GHOSPOT.png' },
-    //     { userid: 17, username: 'user17', HeartPoints: 10, usericon: '/GHOSPOT.png' },
-    //     { userid: 18, username: 'user18', HeartPoints: 10, usericon: '/GHOSPOT.png' },
-    //     { userid: 19, username: 'user19', HeartPoints: 10, usericon: '/GHOSPOT.png' },
-    //     { userid: 20, username: 'user20', HeartPoints: 10, usericon: '/GHOSPOT.png' },
-    // ];
+    //api通さずに実行するならこっちをコメントアウトする
+    const Users = [
+        { user_id: 1, user_name: 'user1', HeartPoints: 100, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 2, user_name: 'user2', HeartPoints: 90, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 3, user_name: 'user3', HeartPoints: 80, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 4, user_name: 'user4', HeartPoints: 70, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 5, user_name: 'user5', HeartPoints: 60, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 6, user_name: 'user6', HeartPoints: 50, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 7, user_name: 'user7', HeartPoints: 40, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 8, user_name: 'user8', HeartPoints: 30, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 9, user_name: 'user9', HeartPoints: 20, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 10, user_name: 'user10', HeartPoints: 10, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 11, user_name: 'user11', HeartPoints: 10, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 12, user_name: 'user12', HeartPoints: 10, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 13, user_name: 'user13', HeartPoints: 10, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 14, user_name: 'user14', HeartPoints: 10, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 15, user_name: 'user15', HeartPoints: 10, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 16, user_name: 'user16', HeartPoints: 10, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 17, user_name: 'user17', HeartPoints: 10, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 18, user_name: 'user18', HeartPoints: 10, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 19, user_name: 'user19', HeartPoints: 10, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+        { user_id: 20, user_name: 'user20', HeartPoints: 10, user_icon: 'https://avatars.githubusercontent.com/u/583231?v=4' },
+    ];
 
     return (
         <Box sx={{ padding: 2, paddingTop: 6 }}>
@@ -47,8 +49,8 @@ const page = async () => {
             >
                 Leaderboard
             </h1>
-            <ShowTop3 users={Users_api} />
-            <Show4thOrLower users={Users_api} />
+            <ShowTop3 users={Users} />
+            <Show4thOrLower users={Users} />
         </Box>
     );
 }
