d53e1c4ae04f9b5045fcce42fab5c7f6fcbe5a7d
tomizawakenshin <kenshin.8799@gmail.com>
2024-06-07 11:39:18 +0900

    interfaceの型定義を追加した

--- app/types.ts
+++ app/types.ts
@@ -1,6 +1,29 @@
+export interface User {
+    user_id: Number;
+    user_name: string;
+    user_icon: string;
+    email: string;
+}
+
 export interface UserInfo {
     user_id: Number;
     user_name: string;
-    HeartPoints: Number;
+    User_HeartPoints: Number;
     user_icon: string;
+    email: string;
+}
+
+export interface PostData {
+    Post: [];
+}
+
+export interface Post {
+    id: Number;
+    topic_id: Number;
+    user_id: Number;
+    post_content: string;
+    created_at: string;
+    updated_at: string;
+    user: User;
+    Post_HeartPoints: Number;
 }
