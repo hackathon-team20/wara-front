c5c20785348e103c91e2566d1cc726b21a93c418
kametsun <kametsun7@gmail>
2024-06-09 11:42:27 +0900

    [add] 修正

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,48 +1,71 @@
-'use client'
-import { Box, Container, Typography } from '@mui/material'
-import Title from '@/app/components/Title'
-import PostForm from '@/app/posts/PostForm'
-import CountDownTimer from '../components/CountDownTimer'
-import { PostData, TopicData } from '../types'
-import { fetchLatestTopic } from '../api'
-import { useEffect, useState } from 'react'
+"use client";
+
+import { Box, Container, Typography } from "@mui/material";
+import Title from "@/app/components/Title";
+import PostForm from "@/app/posts/PostForm";
+import CountDownTimer from "../components/CountDownTimer";
+import { useEffect, useState } from "react";
+import Answer from "../components/Answer";
+
+interface RecentTopic {
+  topic_id: number;
+  topic: string;
+  topic_created_at: Date;
+  topic_image: string;
+  post_id: number | null;
+  post_content: string | null;
+  post_created_at: Date | null;
+}
 
 export default function PostPage() {
-  const [topic, setTopic] = useState<TopicData | undefined>()
+  const [topic, setTopic] = useState<RecentTopic | undefined>();
 
   useEffect(() => {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem("token");
     const fetchLatestTopic = async (token: string | null) => {
-      const res = await fetch(`http://localhost:8000/api/user/recent`, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`
-        },
-        cache: 'no-store'
-      })
-      const Topic: TopicData = await res.json()
-      setTopic(Topic)
-    }
-    fetchLatestTopic(token)
-  }, [])
+      if (token) {
+        const res = await fetch(
+          `https://wara-back-qr9q.onrender.com/api/user/recent`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const Topic: RecentTopic = await res.json();
+        setTopic(Topic);
+      } else {
+      }
+    };
+    fetchLatestTopic(token);
+  }, []);
+
+  if (!topic) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <Container
-      component={'main'}
+      component={"main"}
       maxWidth={false}
       disableGutters
       sx={{
-        marginTop: '15px'
+        marginTop: "15px",
       }}
     >
       <CountDownTimer createdAt={new Date()} />
-      <Box my={'10px'} textAlign={'center'}>
-        <Typography variant={'h3'} fontWeight={'bolder'}>
+      <Box my={"10px"} textAlign={"center"}>
+        <Typography variant={"h3"} fontWeight={"bolder"}>
           投稿する
         </Typography>
       </Box>
-      <Title topic={topic?.topic.topic} image={topic?.topic.image} />
-      <PostForm />
+      <Title topic={topic?.topic} image={topic?.topic_image} />
+      {topic.post_id === null ? (
+        <PostForm />
+      ) : (
+        <Answer post_content={topic.post_content!} />
+      )}
     </Container>
-  )
+  );
 }
