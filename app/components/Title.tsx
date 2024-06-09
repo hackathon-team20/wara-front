2e5c17245751374aee8677317ed22f024177521c
kametsun <kametsun7@gmail>
2024-06-09 14:16:32 +0900

    [fix] posting page

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -1,30 +1,40 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Typography } from "@mui/material";
 
 interface Props {
-  topic: string | undefined
-  image: string | undefined
+  topic: string | undefined;
+  image: string | undefined;
 }
 
 export default function Title(props: Props) {
   return (
-    <Box height={'25vh'} my={'55px'}>
-      <Typography variant={'h4'} component={'h5'} fontWeight={'bold'} textAlign={'center'}>
+    <Box height={"30vh"} my={"55px"}>
+      <Typography
+        variant={"h4"}
+        component={"h5"}
+        fontWeight={"bold"}
+        textAlign={"center"}
+      >
         {props.topic}
       </Typography>
       <Box
-        bgcolor={'gray'}
-        mt={'2px'}
-        display={'flex'}
-        justifyContent={'center'}
-        alignItems={'center'}
-        textAlign={'center'}
+        bgcolor={"gray"}
+        mt={"2px"}
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        textAlign={"center"}
         sx={{
-          height: 'auto',
-          width: '100%'
+          height: "auto",
+          width: "100%",
         }}
       >
-        <Box component={'img'} width={'100%'} height={'auto'} src={props.image}></Box>
+        <Box
+          component={"img"}
+          width={"100%"}
+          height={"auto"}
+          src={props.image}
+        ></Box>
       </Box>
     </Box>
-  )
+  );
 }
